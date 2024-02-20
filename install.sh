7d678d19b66134af5a661a81067510595be972d1
asmi57 <ajs.mail.home@gmail.com>
2024-02-20 02:28:59 -0500

    removed btop

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-modules="betterdiscord btop fish kitty nvim picom rofi scripts starship tmux x11 sway"
+modules="betterdiscord fish kitty nvim picom rofi scripts starship tmux x11 sway"
 
 for module in $modules; do
 	stow -t $HOME $module
