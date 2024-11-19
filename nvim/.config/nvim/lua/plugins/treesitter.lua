0f7055288f4502d7c2cdccd514b061c370afb481
asmi57 <ajs.mail.home@gmail.com>
2024-11-19 11:37:31 -0500

    treesitter objects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
+	dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
 	config = function()
 		require('nvim-treesitter.configs').setup({
 			auto_install = true,
@@ -7,7 +8,20 @@ return {
 				enable = true,
 				additional_vim_regex_highlighting = false,
 			},
-			ensure_installed = { 'vim', 'vimdoc', }
+			ensure_installed = { 'vim', 'vimdoc', },
+			textobjects = {
+				select = {
+					enable = true,
+					keymaps = {
+						['ib'] = "@block.inner",
+						['ab'] = "@block.outer",
+						['il'] = "@loop.inner",
+						['al'] = "@loop.outer",
+						['if'] = "@function.inner",
+						['af'] = "@function.outer",
+					}
+				}
+			}
 		})
 	end
 }
